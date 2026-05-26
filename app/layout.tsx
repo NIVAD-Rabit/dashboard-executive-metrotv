8790ef8a2cadf184f5dc7172eaa12a312ecdffda
Mohammad Denny <mohammaddenny94@gmail.com>
2026-05-26 07:29:52 +0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import type { Metadata } from "next";
+// import { Geist, Geist_Mono } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +24,19 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      lang="id"
+      suppressHydrationWarning
+      // className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      {/* antialiased: biar pinggiran text alus di layar */}
+      <body className="antialiased bg-background text-foreground">
+        <div className="flex h-screen w-full overflow-hidden relative border-2 border-amber-500">
+          {/* Sidebar */}
+          <div></div>
+          {/* End Sidebar */}
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
